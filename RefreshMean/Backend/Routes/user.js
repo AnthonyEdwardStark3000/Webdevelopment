1b5dc36250160bd8d49178356ed15aaf59938236
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-18 14:32:10 +0530

    Authentication issue

--- RefreshMean/Backend/Routes/user.js
+++ RefreshMean/Backend/Routes/user.js
@@ -1,9 +1,26 @@
 //for user authentication implementing user login and signup
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const router = express.Router();
 
 router.post("/signup", function(req, res, next){
-
+  bcrypt.hash(req.body.password, 10).then(hash =>{
+    const user = new User({
+      email: req.body.email,
+      // password: req.body.password bad idea to store it as raw data as anybody can access it, use bcrypt.
+      password: hash
+    });
+  user.save().then(result =>{
+    res.status(201).json({
+      message: 'User created !',
+      result: result
+    });
+  }).catch(err =>{
+    res.status(500).json({
+      error: err
+    })
+  });
 });
-
+});
 module.exports = router;
